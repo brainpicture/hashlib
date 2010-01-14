d79aad2ecc052d1b6a85cb09b18b495854eef618
brainfucker <oleg@emby.ru>
2010-01-14 17:25:45 +0300

    Adding sha1, sha256, sha512 and md6 hashes

--- hashlib.cc
+++ hashlib.cc
@@ -1,47 +1,171 @@
 #include <v8.h>
-//#include "lib/md5/md5.cpp"
-//#include "lib/md5/arc4.h"
+
 #include "lib/md5/md5.c"
 
+#include "lib/sha/shamodule.c"
+#include "lib/sha/sha256module.c"
+#include "lib/sha/sha512module.c"
+
+#include "lib/md6/md6.h"
+#include "lib/md6/md6_compress.c"
+#include "lib/md6/md6_mode.c"
+
+//
+
 #include <iostream>
-#include <string>
 using namespace v8;
+
+Handle<Value>
+sha1(const Arguments& args)
+{
+  HandleScope scope;	
+  using namespace sha1module;
+  String::Utf8Value data(args[0]->ToString());
+  unsigned char digest[40];
+  unsigned char hexdigest[40];
+  SHAobject *sha;
+  sha=new SHAobject;
+  sha_init(sha);
+  sha_update(sha, (unsigned char*) *data, data.length());
+  sha_final(digest, sha);
+  char chr;
+  int i, j;
+  for(i=j=0; i<20; i++) {
+    chr = (digest[i] >> 4) & 0xf;
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
+    chr = (digest[i] & 0xf);
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
+  }
+	     
+  return String::New((char*)hexdigest,40);
+}
+
+
+Handle<Value>
+sha256(const Arguments& args)
+{
+  HandleScope scope;	
+  using namespace sha256module;
+  String::Utf8Value data(args[0]->ToString());
+  unsigned char digest[64];
+  unsigned char hexdigest[64];
+  SHAobject *sha;
+  sha=new SHAobject;
+  sha_init(sha);
+  sha_update(sha, (unsigned char*) *data, data.length());
+  sha_final(digest, sha);
+  char chr;
+  int i, j;
+  for(i=j=0; i<32; i++) {
+    chr = (digest[i] >> 4) & 0xf;
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
+    chr = (digest[i] & 0xf);
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
+  }
+	     
+  return String::New((char*)hexdigest,64);
+}
+
+Handle<Value>
+sha512(const Arguments& args)
+{
+  HandleScope scope;	
+  using namespace sha512module;
+  String::Utf8Value data(args[0]->ToString());
+  unsigned char digest[128];
+  unsigned char hexdigest[128];
+  SHAobject *sha;
+  sha=new SHAobject;
+  sha512_init(sha);
+  sha512_update(sha, (unsigned char*) *data, data.length());
+  sha512_final(digest, sha);
+  char chr;
+  int i, j;
+  for(i=j=0; i<64; i++) {
+    chr = (digest[i] >> 4) & 0xf;
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
+    chr = (digest[i] & 0xf);
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
+  }
+	     
+  return String::New((char*)hexdigest,128);
+}
+
+
+Handle<Value>
+md6(const Arguments& args)
+{
+  HandleScope scope;	
+  
+  String::Utf8Value data(args[0]->ToString());
+  
+  int len(32);
+  if (!args[1]->IsUndefined()) {
+    len=args[1]->ToInteger()->Value();
+  }
+  unsigned char digest[len];
+  unsigned char hexdigest[len];
+  md6_hash(len*8, (unsigned char*) *data, data.length(), digest);
+  char chr;
+  int i, j;
+  int half_len=len/2;
+  if (len%2!=0) half_len++;
+  
+  for(i=j=0; i<half_len; i++) {
+    chr = (digest[i] >> 4) & 0xf;
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
+    chr = (digest[i] & 0xf);
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
+  }
+	     
+  return String::New((char*)hexdigest,len);
+}
 
 
 Handle<Value>
 md5(const Arguments& args)
 {
-  HandleScope scope;	
+  HandleScope scope;
   
   String::Utf8Value data(args[0]->ToString());
-
   md5_state_t mdContext;
   unsigned char digest[16];
   unsigned char hexdigest[32];
-  int i, j;
 
   /* make an hash */
   md5_init(&mdContext);
-  md5_append(&mdContext,(unsigned char*) *data,data.length());
+  md5_append(&mdContext,(unsigned char*)*data,data.length());
   md5_finish(&mdContext, digest);
-
-  /* Make hex version of the digest */
+  
+  int i, j;
   for(i=j=0; i<16; i++) {
-    char c;
-    c = (digest[i] >> 4) & 0xf;
-    c = (c>9) ? c+'a'-10 : c + '0';
-    hexdigest[j++] = c;
-    c = (digest[i] & 0xf);
-    c = (c>9) ? c+'a'-10 : c + '0';
-    hexdigest[j++] = c;
+  	char chr;
+    chr = (digest[i] >> 4) & 0xf;
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
+    chr = (digest[i] & 0xf);
+    chr = (chr>9) ? chr+'a'-10 : chr + '0';
+    hexdigest[j++] = chr;
   }
   
   return String::New((char*)hexdigest,32);
 }
 
-
 extern "C" void init (Handle<Object> target)
 {
   HandleScope scope;
   target->Set(String::New("md5"), FunctionTemplate::New(md5)->GetFunction());
+  target->Set(String::New("md6"), FunctionTemplate::New(md6)->GetFunction());
+  target->Set(String::New("sha"), FunctionTemplate::New(sha1)->GetFunction());
+  target->Set(String::New("sha1"), FunctionTemplate::New(sha1)->GetFunction());
+  target->Set(String::New("sha256"), FunctionTemplate::New(sha256)->GetFunction());
+  target->Set(String::New("sha512"), FunctionTemplate::New(sha512)->GetFunction());
 }
--- test.js
+++ test.js
@@ -1,24 +1,85 @@
 var hashlib = require("./build/default/hashlib");
 var sys = require("sys");
+var md5 = require("./test/md5");
 
-sys.puts(hashlib.md5([2,4,6]));
-var md5 = require("./lib/md5");
+if (hashlib.md5('test')=='098f6bcd4621d373cade4e832627b4f6')
+	sys.puts('test 1 PASSED');
+else
+	sys.puts('test 1 FAILS');
+if (hashlib.sha1('test')=='a94a8fe5ccb19ba61c4c0873d391e987982fbbd3')
+	sys.puts('test 2 PASSED');
+else
+	sys.puts('test 2 FAILS');
+if (hashlib.sha256('test')=='9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08')
+	sys.puts('test 3 PASSED');
+else
+	sys.puts('test 3 FAILS');
+if (hashlib.sha512('test')=='ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff')
+	sys.puts('test 4 PASSED');
+else
+	sys.puts('test 4 FAILS');
+if (hashlib.md6('test')=='e2e671a86f079441639ff18928092c6e')
+	sys.puts('test 5 PASSED');
+else
+	sys.puts('test 5 FAILS');
+if (hashlib.md6('test',40)=='27ea128154b4934ae2bd7f9dac53cf81da0002cc')
+	sys.puts('test 6 PASSED');
+else
+	sys.puts('test 6 FAILS');
 
+// C++ md5
 var m1=new Date().getTime();
 for(i=0;i<100000;i++) {
 	h=hashlib.md5('EdPy2H71Q1MjTzkuRxAr1CJWs2ZapZEuaY3XwJL8mpxaTBLWZPkw1yakKLv2r79eHmNQ1m2Cc6PErAkH5FR3Nmd011F09LCas76Z'+String(i));
 }
 var m2=new Date().getTime();
 var c=m2-m1;
-sys.puts('C++ module result is: '+(c));
+sys.puts('C++ md5 result is: '+(c));
 
+// JS md5
 var m1=new Date().getTime();
 for(i=0;i<100000;i++) {
 	h=md5.md5('EdPy2H71Q1MjTzkuRxAr1CJWs2ZapZEuaY3XwJL8mpxaTBLWZPkw1yakKLv2r79eHmNQ1m2Cc6PErAkH5FR3Nmd011F09LCas76Z'+String(i));
 }
 var m2=new Date().getTime();
 var js=m2-m1;
-sys.puts('JS module result is: '+(js));
+sys.puts('JS md5 result is: '+(js));
+if (c<js) sys.puts('C++ module faster than JS in '+(js/c)+' times');
+else if (c>js) sys.puts('JS module faster than C++ in '+(c/j)+' times');
+sys.puts('-----------');
 
-if (c<js) sys.puts('C module faster than JS in '+(js/c)+' times');
-else if (c>js) sys.puts('JS module faster than C in '+(c/j)+' times');
+// C++ md6
+var m1=new Date().getTime();
+for(i=0;i<100000;i++) {
+	h=hashlib.md6('EdPy2H71Q1MjTzkuRxAr1CJWs2ZapZEuaY3XwJL8mpxaTBLWZPkw1yakKLv2r79eHmNQ1m2Cc6PErAkH5FR3Nmd011F09LCas76Z'+String(i));
+}
+var m2=new Date().getTime();
+var c=m2-m1;
+sys.puts('C++ md6 result is: '+(c));
+
+// C++ sha1
+var m1=new Date().getTime();
+for(i=0;i<100000;i++) {
+	h=hashlib.sha1('EdPy2H71Q1MjTzkuRxAr1CJWs2ZapZEuaY3XwJL8mpxaTBLWZPkw1yakKLv2r79eHmNQ1m2Cc6PErAkH5FR3Nmd011F09LCas76Z'+String(i));
+}
+var m2=new Date().getTime();
+var c=m2-m1;
+sys.puts('C++ sha1 result is: '+(c));
+
+// C++ sha256
+var m1=new Date().getTime();
+for(i=0;i<100000;i++) {
+	h=hashlib.sha256('EdPy2H71Q1MjTzkuRxAr1CJWs2ZapZEuaY3XwJL8mpxaTBLWZPkw1yakKLv2r79eHmNQ1m2Cc6PErAkH5FR3Nmd011F09LCas76Z'+String(i));
+}
+var m2=new Date().getTime();
+var c=m2-m1;
+sys.puts('C++ sha256 result is: '+(c));
+
+// C++ sha512
+var m1=new Date().getTime();
+for(i=0;i<100000;i++) {
+	h=hashlib.sha512('EdPy2H71Q1MjTzkuRxAr1CJWs2ZapZEuaY3XwJL8mpxaTBLWZPkw1yakKLv2r79eHmNQ1m2Cc6PErAkH5FR3Nmd011F09LCas76Z'+String(i));
+}
+var m2=new Date().getTime();
+var c=m2-m1;
+sys.puts('C++ sha512 result is: '+(c));
