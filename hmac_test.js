ed4009f59f2fddab86c94e3a8df192f2ce3580ed
David Rice <me@davidjrice.co.uk>
2010-02-15 03:54:16 +0000

    Using official test vectors.. doesn't seem to work though

--- hmac_test.js
+++ hmac_test.js
@@ -4,7 +4,13 @@ var md5 = require("./test/md5");
 
 process.chdir(__dirname);
 
-sys.puts(hashlib.hmac_sha1('testsjdfhskjdfhskjfhsajkfhsakf','34'));
-sys.puts(hashlib.hmac_sha1('wow','smsm'));
-sys.puts(hashlib.hmac_sha1('HEY MAN','man man'));
-sys.puts(hashlib.hmac_sha1('HEY MAN','man man2'));
+// TEST VECTORS 
+// http://www.faqs.org/rfcs/rfc2202.html
+//
+// Official test vectors.. don't appear to be working. Unless there is another text encoding step.
+// i.e. ascii to hex... or something
+//
+
+result = hashlib.hmac_sha1('Hi There', 8, '0x0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b', 20);
+sys.puts(result);
+sys.puts(sys.inspect(result));
